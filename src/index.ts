58cd982fe20393fcee3473e9391221409fbf13fd
GL <hubbawubba64@gmail.com>
2025-09-25 14:42:14 -0500

    gitignore node_modules, cull node_modules

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 console.log("starting...");
 const canvas = document.getElementById("mainCanvas") as HTMLCanvasElement;
-const title = document.getElementById("title");
-const context = canvas.getContext("2d") as HTMLCanvasContext;
+var title = document.getElementById("title");
+const context = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 canvas.width  = window.innerWidth; 
 canvas.height = window.innerHeight;
-title.text = "Welcome";
 context.font = " 20px Arial ";
 context.strokeText("Test", 10, 50);
